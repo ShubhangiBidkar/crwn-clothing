115ac48d9eac8374a55eef9964691799a9415e80
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-04-01 22:31:38 -0500

    adding navigation bar and linking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { Route,Routes } from "react-router-dom";
-import Home from "./routes/home/home.component"
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () =>{
+  return(
+    <div>
+      <h1>I m inside Shopping</h1>
+    </div>
+  );
+}
 
 
 const App = () => {
@@ -10,7 +18,12 @@ const App = () => {
   
   return (
     <Routes>
-      <Route path='/' element={<Home/>}></Route>
+      <Route path='/' element={<Navigation/>}>
+        {/* index will tell that along with the parent element also render the Home component  in place where we have <Outlet in Navigation*/}
+        {/* with path we say that after the / match the path what is given as and render that component in place where we have <Outlet in Navigation> */}
+        <Route index element={<Home/>}></Route>
+        <Route path='shop' element={<Shop/>}></Route>
+      </Route>
     </Routes>
    
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet ,Link } from "react-router-dom";
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+const Navigation = () =>{
+    return(
+        <>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <CrwnLogo className='logo' >Logo</CrwnLogo>
+                </Link>
+                
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>Shop</Link>
+                </div>
+            </div>
+            {/* This will render the nested route here which we are nesting in App.js */}
+            <Outlet/>
+            
+        </>
+    );
+}
+
+
+export default Navigation;
