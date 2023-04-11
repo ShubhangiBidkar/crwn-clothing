ffe34e3c0bb685bd518de460174ded25319097d8
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-04-11 14:44:23 -0500

    created nested routes for shop and created Category com[ponent

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         {/* index will tell that along with the parent element also render the Home component  in place where we have <Outlet in Navigation*/}
         {/* with path we say that after the / match the path what is given as and render that component in place where we have <Outlet in Navigation> */}
         <Route index element={<Home/>}></Route>
-        <Route path='shop' element={<Shop/>}></Route>
+        <Route path='shop/*' element={<Shop/>}></Route>
         <Route path='sign-in' element={<Authentication/>}></Route>
         <Route path='checkout' element={<Checkout />}></Route>
       </Route>
