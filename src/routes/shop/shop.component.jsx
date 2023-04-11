dd8b7a59b9b919d78adcbe9f68a36d85d6a4086a
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-04-11 13:14:06 -0500

    Updated the shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,27 +1,21 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../context/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-import './shop.styles.scss'
+import { useContext } from 'react';
 
-const Shop = () =>{
-    const {categoriesMap} = useContext(CategoriesContext);
-     //console.log(products)
-    return(
-        <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
-        
-    )
-}
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+import { CategoriesContext } from '../../context/categories.context';
+import './shop.styles.scss';
+
+const Shop = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </div>
+  );
+};
 
 export default Shop;
-
