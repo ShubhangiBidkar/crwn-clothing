3f7fde60da7410c57d26c085f5311bdef81fd89c
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-04-15 08:52:17 -0500

    removing provider reference for user

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,23 @@ import App from './App'
 import { UserProvider } from './contexts/user.context'
 import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart-context'
+import {Provider} from 'react-redux';
+import {store} from './store/store';
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    {/* any component inside of this user provider nested deep within the app can access the context value inside of the provider itself. */}
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      {/* any component inside of this user provider nested deep within the app can access the context value inside of the provider itself. */}
+        {/* <UserProvider> */}
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
