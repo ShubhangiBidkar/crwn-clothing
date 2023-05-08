9f061f7fe30f9bb48c7210796f643238d2490b43
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-05-08 15:27:46 -0500

    Added some comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,32 @@
 // we create our store and bring everything together
-import { compose,applyMiddleware } from "redux";
+import { compose, applyMiddleware } from "redux";
 // bcoz creatStore is deprecated
 import { legacy_createStore as createStore } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reduce";
-import { persistStore,persistReducer } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 // import persistReducer from "redux-persist/es/persistReducer";
 
-
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+// Our middleware will be applied when we are inside development
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
+// if we want to use redux devtools
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-
-const  persistConfig ={
-  key:'root',
+const persistConfig = {
+  key: "root",
   storage,
-  blacklist:['user']
-}
+  blacklist: ["user"],
+};
 
-const persistedReducer = persistReducer(persistConfig,rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // const middleWares = [logger];
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
