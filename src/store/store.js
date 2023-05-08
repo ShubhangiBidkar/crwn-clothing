adb1fca3ec7683aeff79e56824e2eb3f103d2688
shubhangi.bidkar <shubhangi.bidkar@gmail.com>
2023-05-08 15:15:45 -0500

    Added comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,40 +1,47 @@
 // we create our store and bring everything together
-import { compose,applyMiddleware } from "redux";
+import { compose, applyMiddleware } from "redux";
 // bcoz creatStore is deprecated
 import { legacy_createStore as createStore } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reduce";
-import { persistStore,persistReducer } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import persistReducer from "redux-persist/es/persistReducer";
+// Persist is used to persisrt the reducers in local storage
 
 const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-      return next(action);
-    }
-  
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-  
-    next(action);
-  
-    console.log('next state: ', store.getState());
-  };
+  if (!action.type) {
+    return next(action);
+  }
 
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
 
-const  persistConfig ={
-  key:'root',
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+// Persist configuration object
+const persistConfig = {
+  //key is the part that we want to start with and Root just says, I want you to persist the whole
+  // thing
+  key: "root",
+  // what do we want to store this into?
   storage,
-  blacklist:['user']
-}
+  blacklist: ["user"],
+};
 
-const persistedReducer = persistReducer(persistConfig,rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [loggerMiddleware];
 
 const composedMiddleWares = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(persistedReducer,undefined ,composedMiddleWares);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedMiddleWares
+);
 
 export const persistor = persistStore(store);
